ba05621d862b5293a9bf5457994e75d7bee2116b
Rodrigo Fontanella <fontanellarodrigo13@gmail.com>
2021-05-13 04:29:04 -0300

    first commit

---
+++ backend/src/main/java/io/fontanella/backend/User.java
@@ -0,0 +1,21 @@
+package io.fontanella.backend;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String Nome;
+    private String Email;
+    private String Senha;
+    private String Lanche;
+}
